bd736dee2ebe6a3162eabb0ddc08615e580118fe
smartshaila <smartshaila@gmail.com>
2017-09-28 17:56:52 -0500

    added colors model and db seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170928130121) do
+ActiveRecord::Schema.define(version: 20170928133932) do
+
+  create_table "colors", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "provider"
