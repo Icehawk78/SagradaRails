9c87c66646b2c182ee8c603e081300ae3d1cff0a
Nicholas <i.am@aweso.ms>
2017-11-12 00:38:35 -0600

    Fixed line spacing

--- app/models/cell.rb
+++ app/models/cell.rb
@@ -13,18 +13,19 @@ class Cell < ApplicationRecord
   end
 
   def valid_dice placed_game_dice
-    occupied
-    or pip != placed_game_dice.pip
-    or color != placed_game_dice.color
-    or orthogonals.where(occupied: true).where(["pip = :pip or color = :color", {pip: placed_game_dice.pip, color: placed_game_dice.color}]).present?
-    or (player.is_board_empty? and
-        (coordinates.x == 0
-        or coordinates.y == 0
-        or coordinates.x == 4
-        or coordinates.y == 3))
-    or (not player.is_board_empty? and (
-      orthogonals.where(occupied: true).present?
-      or diagonals.where(occupied: true).present?
-    ))
+    occupied or
+      pip != placed_game_dice.pip or
+      color != placed_game_dice.color or
+      orthogonals.where(occupied: true).where(["pip = :pip or color = :color", {pip: placed_game_dice.pip, color: placed_game_dice.color}]).present? or
+      (player.is_board_empty? and
+          (coordinates.x == 0 or
+              coordinates.y == 0 or
+              coordinates.x == 4 or
+              coordinates.y == 3)
+      ) or
+      (not player.is_board_empty? and
+          (orthogonals.where(occupied: true).present? or
+              diagonals.where(occupied: true).present?)
+      )
   end
 end
